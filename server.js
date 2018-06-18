bbcd54bf02ee1959abb0e6e75a6d5ff635c0d56f
igorgcosta <igorgabriel@outlook.com>
2018-06-18 11:32:39 -0300

    correção de CORS

--- server.js
+++ server.js
@@ -31,11 +31,13 @@ if(config.util.getEnv('NODE_ENV') !== 'test') {
 
 app.use(cors());
 app.all('*', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, crossDomain');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, crossDomain');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    
     if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
+        res.sendStatus(200);
     } else {
       next();
     }
